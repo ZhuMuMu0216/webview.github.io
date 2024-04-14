846de3c65e124b672ac6d0802470fca279e7ed15
ZhuMuMu0216 <122190080+ZhuMuMu0216@users.noreply.github.com>
2024-04-14 16:14:25 +0800

    new_script_dataviz

--- script.js
+++ script.js
@@ -8,49 +8,20 @@ document.getElementById('csvFileInput').addEventListener('change', function(even
     reader.onload = function(e) {
         const text = e.target.result;
         const data = d3.csvParse(text);
-        drawChart(data);
+
+        // 使用正确的列名
+        const sumByYear = data.reduce((acc, row) => {
+            const date = new Date(row['startDate']);  // 使用列名'startDate'提取日期
+            const year = date.getFullYear();  // 从日期中提取年份
+            const value = parseFloat(row['value']);  // 使用列名'value'提取数值
+            if (!acc[year]) {
+                acc[year] = 0;
+            }
+            acc[year] += value;  // 将值累加到对应年份
+            return acc;
+        }, {});
+
+        console.log(sumByYear);
     };
     reader.readAsText(file);
 });
-
-function drawChart(data) {
-    const svg = d3.select('svg');
-    svg.selectAll("*").remove();  // 清除之前的可视化
-
-    const margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom;
-
-    const x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        y = d3.scaleLinear().rangeRound([height, 0]);
-
-    const g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    x.domain(data.map(d => d.columnX));
-    y.domain([0, d3.max(data, d => +d.columnY)]);
-
-    g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    g.append("g")
-        .attr("class", "axis axis--y")
-        .call(d3.axisLeft(y).ticks(10))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Value");
-
-    g.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", d => x(d.columnX))
-        .attr("y", d => y(+d.columnY))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y(+d.columnY));
-}
